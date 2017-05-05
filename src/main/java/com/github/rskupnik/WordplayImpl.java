ad3a76d89e647844e19a0ca9633af3a3b4bf8de5
Myzreal <r.skupnik@gmail.com>
2017-05-05 20:32:21 +0200

    A very naive implementation to make the tests pass

--- src/main/java/com/github/rskupnik/WordplayImpl.java
+++ src/main/java/com/github/rskupnik/WordplayImpl.java
@@ -1,20 +1,44 @@
 package com.github.rskupnik;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordplayImpl implements Wordplay {
+
+    private Map<String, Boolean> booleanVariablesMap = new HashMap<>();
 
     @Override
     public String process(String input) {
-        return null;
+        int openingBracketIndex = input.indexOf('{');
+        int closingBracketIndex = input.indexOf('}');
+
+        if (openingBracketIndex == -1 || closingBracketIndex == -1)
+            return null;
+
+        String expression = input.substring(openingBracketIndex+1, closingBracketIndex);
+
+        if (!expression.contains("?") && !expression.contains("|"))
+            return null;
+
+        String variable = expression.split("\\?")[0].trim();
+        String rest = expression.split("\\?")[1].substring(1);
+        String[] split = rest.split("\\s\\|\\s");
+        String first = split[0];
+        String second = split[1];
+
+        String result = booleanVariablesMap.get(variable) != null && booleanVariablesMap.get(variable) == true ? first : second;
+
+        return input.substring(0, openingBracketIndex) + result + input.substring(closingBracketIndex+1);
     }
 
     @Override
     public void reset() {
-
+        booleanVariablesMap.clear();
     }
 
     @Override
     public void setVariable(String var, boolean value) {
-
+        booleanVariablesMap.put(var, value);
     }
 
     @Override
