82710110459ea275a65a80bfb32792158d5c1fe4
Myzreal <r.skupnik@gmail.com>
2018-04-07 13:18:41 +0200

    Add a test for headers and code sections together

--- src/test/groovy/com/github/rskupnik/wordplay/WordplayPreprocessingTest.groovy
+++ src/test/groovy/com/github/rskupnik/wordplay/WordplayPreprocessingTest.groovy
@@ -1,5 +1,8 @@
 package com.github.rskupnik.wordplay
 
+import com.github.rskupnik.wordplay.output.MetaMap
+import com.github.rskupnik.wordplay.output.MetaObject
+import com.github.rskupnik.wordplay.output.WordplayOutput
 import spock.lang.Specification
 
 class WordplayPreprocessingTest extends Specification {
@@ -21,6 +24,32 @@ class WordplayPreprocessingTest extends Specification {
         final Map<String, String> headers = wordplay.extractHeaders(input)
 
         then:
+        headers.size() == 2
+        headers.get("key1").equals("value1")
+        headers.get("key2").equals("value2")
+    }
+
+    def "should emit map with header section present"() {
+        given:
+        final String input = "Irrelevant text\n" +
+                "\$\n" +
+                "<m key:val|key2:val2\n" +
+                "!\$\n" +
+                "key value1\n" +
+                "key2 value2"
+
+        when:
+        final Map<String, String> headers = wordplay.extractHeaders(input)
+        final WordplayOutput output = wordplay.process(input)
+
+        then:
+        List<MetaObject> metaObjects = output.getMetaObjects()
+        metaObjects.size() == 1
+        MetaObject mobj = metaObjects.get(0)
+        mobj instanceof MetaMap
+        MetaMap mmap = (MetaMap) mobj
+        mmap.getString("key").equals("val")
+        mmap.getString("key2").equals("val2")
         headers.size() == 2
         headers.get("key1").equals("value1")
         headers.get("key2").equals("value2")
