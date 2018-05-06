ee8b4c55c521c1a129d5ad7fa413557e67692c95
Myzreal <r.skupnik@gmail.com>
2018-05-06 13:54:18 +0200

    Allow '#' symbol in anchored objects

--- src/main/java/com/github/rskupnik/wordplay/internal/processors/EmissionProcessor.java
+++ src/main/java/com/github/rskupnik/wordplay/internal/processors/EmissionProcessor.java
@@ -31,7 +31,7 @@ import java.util.regex.Pattern;
 
 public final class EmissionProcessor {
 
-    private static final Pattern PATTERN_ANCHORED = Pattern.compile("\\{<\\s*([\\w:\\|]+)\\s([\\w\\s]+)\\s*\\}");
+    private static final Pattern PATTERN_ANCHORED = Pattern.compile("\\{<\\s*([\\w#:\\|]+)\\s([\\w\\s]+)\\s*\\}");
     private static final Pattern PATTERN_SUBGROUP = Pattern.compile("(\\w+):(\\w+)(\\?\\=\\|)?");
     private static final Pattern PATTERN_TOKEN = Pattern.compile("\\$@s\\d+\\s");
     private static final String TOKEN_START = "$@s";
